8296ac5fd5ceafdc3c1e599e10418228a87f4d62
Murgesh Ekunde <mekunde@gmail.com>
2022-09-07 17:01:11 +0530

    first Buil

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const countdown=document.querySelector(".countdown") //selected element
+
+let time=10;                                        //countdown initialisation
+
+countdown.innerHTML=time--;                         //callback pain (nested callback)
+setTimeout(()=>{
+    countdown.innerHTML=time--;
+    setTimeout(()=>{
+        countdown.innerHTML=time--;
+        setTimeout(()=>{
+            countdown.innerHTML=time--;
+            setTimeout(()=>{
+                countdown.innerHTML=time--;
+                setTimeout(()=>{
+                    countdown.innerHTML=time--;
+                    setTimeout(()=>{
+                        countdown.innerHTML=time--;
+                        setTimeout(()=>{
+                            countdown.innerHTML=time--;
+                            setTimeout(()=>{
+                                countdown.innerHTML=time--;
+                                setTimeout(()=>{
+                                    countdown.innerHTML=time--;
+                                    setTimeout(()=>{
+                                        countdown.innerHTML=time--;
+                                        countdown.innerHTML="Happy Independence Day🎉"
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000);
+},1000)
